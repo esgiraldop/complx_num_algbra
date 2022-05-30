b883e463aa960785fe912658c5ebd1c405267cdc
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-05-30 16:40:40 -0500

    Creatd funcs 4 addin up n subtrctng cmplx and real prts

---
+++ ask_usr.py
@@ -0,0 +1,4 @@
+''' Tasks to do:
+    - Check nothing other than digits are entered by the user
+    - Check input is of the form "n + mi", where "n" is the real part and "m" the complex part
+    - The user should be able to enter fractions or decimals and get the answer in both formats'''
--- calculator.py
+++ calculator.py
@@ -1,19 +1,38 @@
 def real_complx_sepration(num):
-    real_prt = num_1.split('+')[0]
-    complx_part = num_1.split('+')[1].split('i')[0]
+    # NOTE TO MYSELF: what if there are decimals entered?
+    real_prt = num.split('+')[0]
+    complx_part = num.split('+')[1].split('i')[0]
     return real_prt, complx_part
 
-def real_sum(num_1, num_2):
+def decimal_2_frac(num):
+    ''' Function to transform from decimal to fraction'''
     pass
-    return result
+    return frac
 
-def complx_sum(num_1, num_2):
-    pass
-    return result
+def format_floatnum(num):
 
-def real_subs(num_1, num_2):
-    pass
-    return result
+    if num - int(num) == 0:
+        return int(num)
+
+    return num
+
+def real_complx_sum(num_1, num_2):
+    # NOTE TO MYSELF: What if there are decimals?
+    real_prt1, complx_prt1 = real_complx_sepration(num_1)
+    real_prt2, complx_prt2 = real_complx_sepration(num_2)
+    real_prt = format_floatnum(float(real_prt1) + float(real_prt2))
+    complx_prt = format_floatnum(float(complx_prt1) + float(complx_prt2))
+    # Formatting numbers. If there is a number like "5.0i", it is formatted to "5i"
+    return str(real_prt) + ' + ' + str(complx_prt) + 'i'
+
+def real_complx_subs(num_1, num_2):
+    # NOTE TO MYSELF: What if there are decimals?
+    real_prt1, complx_prt1 = real_complx_sepration(num_1)
+    real_prt2, complx_prt2 = real_complx_sepration(num_2)
+    real_prt = format_floatnum(float(real_prt1) - float(real_prt2))
+    complx_prt = format_floatnum(float(complx_prt1) - float(complx_prt2))
+    # Formatting numbers. If there is a number like "5.0i", it is formatted to "5i"
+    return str(real_prt) + ' + ' + str(complx_prt) + 'i'
 
 def complx_subs(num_1, num_2):
     pass
@@ -28,8 +47,12 @@ def real_complx_div(num_1, num_2):
     return result
 
 if __name__ == '__main__':
-    num_1 = '4+3i'
+    num_1 = '4+8i'
     num_2 = '5+3i'
-    real_prt, complx_part = real_complx_sepration(num_1)
-    print('The real part is: ', real_prt)
-    print('The complex part is: ', complx_part)
+    # real_prt1, complx_part1 = real_complx_sepration(num_1)
+    # print('The real part is: ', real_prt)
+    # print('The complex part is: ', complx_part)
+    result = real_complx_sum(num_1, num_2)
+    print('The result of the sum is: ', result)
+    result = real_complx_subs(num_1, num_2)
+    print('The result the subtraction is: ', result)
