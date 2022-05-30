05eda6e95f593d38cb803ab886a6a385374514c2
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-05-30 15:55:18 -0500

    Finshd func 4 sepratng complx and real prts

--- calculator.py
+++ calculator.py
@@ -1,8 +1,6 @@
-num_1 = '4+3i'
-num_2 = '5+3i'
-
-def complx_sepration(num):
-    pass
+def real_complx_sepration(num):
+    real_prt = num_1.split('+')[0]
+    complx_part = num_1.split('+')[1].split('i')[0]
     return real_prt, complx_part
 
 def real_sum(num_1, num_2):
@@ -28,3 +26,10 @@ def real_complx_mult(num_1, num_2):
 def real_complx_div(num_1, num_2):
     pass
     return result
+
+if __name__ == '__main__':
+    num_1 = '4+3i'
+    num_2 = '5+3i'
+    real_prt, complx_part = real_complx_sepration(num_1)
+    print('The real part is: ', real_prt)
+    print('The complex part is: ', complx_part)
