c2cc77bf0d983d04a3ddd39cd5681ca6f0c24962
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-05-29 22:25:04 -0500

    Added schmas of funcs 2 oprate complx nums

---
+++ calculator.py
@@ -0,0 +1,30 @@
+num_1 = '4+3i'
+num_2 = '5+3i'
+
+def complx_sepration(num):
+    pass
+    return real_prt, complx_part
+
+def real_sum(num_1, num_2):
+    pass
+    return result
+
+def complx_sum(num_1, num_2):
+    pass
+    return result
+
+def real_subs(num_1, num_2):
+    pass
+    return result
+
+def complx_subs(num_1, num_2):
+    pass
+    return result
+
+def real_complx_mult(num_1, num_2):
+    pass
+    return result
+
+def real_complx_div(num_1, num_2):
+    pass
+    return result
