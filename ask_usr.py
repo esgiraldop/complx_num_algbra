8e64027cf187e2da15608a6bf5d1c4ae3cf4213c
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-05-30 21:16:08 -0500

    Enhanced func calculator.real_complx_sepration()
    Working now for some extra cases with '-' signs and decimals,
    but there are still some things to enhance, as the function
    still produces errors

--- ask_usr.py
+++ ask_usr.py
@@ -1,4 +1,6 @@
 ''' Tasks to do:
     - Check nothing other than digits are entered by the user
     - Check input is of the form "n + mi", where "n" is the real part and "m" the complex part
-    - The user should be able to enter fractions or decimals and get the answer in both formats'''
+    - The user should be able to enter fractions or decimals and get the answer in both formats
+    - Format the input so there are not whitespaces between terms and/or numbers
+    - Check there are only two signs: One for the real part and one for the complex part'''
--- calculator.py
+++ calculator.py
@@ -1,7 +1,19 @@
+import re
+
+def search_middle_sign(num):
+    sign = re.search(r'\d*\.?\d*(\+|-)\d*\.?\d*i', num).groups()[0]
+    return sign
+
 def real_complx_sepration(num):
     # NOTE TO MYSELF: what if there are decimals entered?
-    real_prt = num.split('+')[0]
-    complx_part = num.split('+')[1].split('i')[0]
+    middle_sign = search_middle_sign(num)
+    if num.count(middle_sign) == 1:
+        real_prt = num.split(middle_sign)[0]
+        complx_part = num.split(middle_sign)[1].split('i')[0]
+    if num.count(middle_sign) == 2:
+        real_prt = num.split(middle_sign)[1]
+        complx_part = num.split(middle_sign)[2].split('i')[0]
+
     return real_prt, complx_part
 
 def decimal_2_frac(num):
@@ -10,6 +22,11 @@ def decimal_2_frac(num):
     return frac
 
 def format_floatnum(num):
+    '''
+        Function for formatting numbers. If there is a number like "5.0i", it is formatted to "5i"
+        :param num:
+        :return:
+    '''
     if num - int(num) == 0:
         return int(num)
 
@@ -30,7 +47,8 @@ def real_complx_subs(num_1, num_2):
     real_prt2, complx_prt2 = real_complx_sepration(num_2)
     real_prt = format_floatnum(float(real_prt1) - float(real_prt2))
     complx_prt = format_floatnum(float(complx_prt1) - float(complx_prt2))
-    # Formatting numbers. If there is a number like "5.0i", it is formatted to "5i"
+
+    # NOTE TO MYSELF: What if there is a result with a '-' as the middle sign?
     return str(real_prt) + ' + ' + str(complx_prt) + 'i'
 
 def real_complx_mult(num_1, num_2):
@@ -43,21 +61,25 @@ def real_complx_mult(num_1, num_2):
     real_prt = term1 + term4
     complx_prt = term2 + term3
 
+    # NOTE TO MYSELF: What if there is a result with a '-' as the middle sign?
     return str(real_prt) + ' + ' + str(complx_prt) + 'i'
 
 def real_complx_div(num_1, num_2):
-    pass
+    real_prt2, complx_prt2 = real_complx_sepration(num_2)
+    complx_conjugate = str(real_prt) + ' + ' + str(complx_prt) + 'i'
     return result
 
 if __name__ == '__main__':
     num_1 = '4+8i'
     num_2 = '5+3i'
-    # real_prt1, complx_part1 = real_complx_sepration(num_1)
-    # print('The real part is: ', real_prt)
-    # print('The complex part is: ', complx_part)
-    result = real_complx_sum(num_1, num_2)
-    print('The result of the sum is: ', result)
-    result = real_complx_subs(num_1, num_2)
-    print('The result the subtraction is: ', result)
-    result = real_complx_mult(num_1, num_2)
-    print('The result the multiplication is: ', result)
+    num_1 = '-4-8i'
+    real_prt1, complx_part1 = real_complx_sepration(num_1)
+    print('The real part is: ', real_prt1)
+    print('The complex part is: ', complx_part1)
+    print('The middle sign is: ', search_middle_sign(num_1))
+    # result = real_complx_sum(num_1, num_2)
+    # print('The result of the sum is: ', result)
+    # result = real_complx_subs(num_1, num_2)
+    # print('The result of the subtraction is: ', result)
+    # result = real_complx_mult(num_1, num_2)
+    # print('The result of the multiplication is: ', result)
