ab985666c47f9cca67ad3a6fe031bd224dad1068
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-05-31 22:01:00 -0500

    Fixed bug when separating real and complx prts

--- calculator.py
+++ calculator.py
@@ -5,15 +5,9 @@ def search_middle_sign(num):
     return sign
 
 def real_complx_sepration(num):
-    # NOTE TO MYSELF: what if there are decimals entered?
-    middle_sign = search_middle_sign(num)
-    if num.count(middle_sign) == 1:
-        real_prt = num.split(middle_sign)[0]
-        complx_part = num.split(middle_sign)[1].split('i')[0]
-    if num.count(middle_sign) == 2:
-        real_prt = num.split(middle_sign)[1]
-        complx_part = num.split(middle_sign)[2].split('i')[0]
-
+    pattern = r'((?:\+|-)?\d*\.?\d*)((?:\+|-)?\d*\.?\d*)i'
+    real_prt = re.search(pattern, num).groups()[0]
+    complx_part = re.search(pattern, num).groups()[1]
     return real_prt, complx_part
 
 def decimal_2_frac(num):
@@ -32,14 +26,21 @@ def format_floatnum(num):
 
     return num
 
+def multply_signs(num, sign):
+
+    return num
+
 def real_complx_sum(num_1, num_2):
     # NOTE TO MYSELF: What if there are decimals?
     real_prt1, complx_prt1 = real_complx_sepration(num_1)
     real_prt2, complx_prt2 = real_complx_sepration(num_2)
     real_prt = format_floatnum(float(real_prt1) + float(real_prt2))
     complx_prt = format_floatnum(float(complx_prt1) + float(complx_prt2))
-    # Formatting numbers. If there is a number like "5.0i", it is formatted to "5i"
-    return str(real_prt) + ' + ' + str(complx_prt) + 'i'
+
+    if str(complx_prt)[0] != '-':
+        return str(real_prt) + '+' + str(complx_prt) + 'i'
+
+    return str(real_prt) + str(complx_prt) + 'i'
 
 def real_complx_subs(num_1, num_2):
     # NOTE TO MYSELF: What if there are decimals?
@@ -48,8 +49,10 @@ def real_complx_subs(num_1, num_2):
     real_prt = format_floatnum(float(real_prt1) - float(real_prt2))
     complx_prt = format_floatnum(float(complx_prt1) - float(complx_prt2))
 
-    # NOTE TO MYSELF: What if there is a result with a '-' as the middle sign?
-    return str(real_prt) + ' + ' + str(complx_prt) + 'i'
+    if str(complx_prt)[0] != '-':
+        return str(real_prt) + '+' + str(complx_prt) + 'i'
+
+    return str(real_prt) + str(complx_prt) + 'i'
 
 def real_complx_mult(num_1, num_2):
     real_prt1, complx_prt1 = real_complx_sepration(num_1)
@@ -61,25 +64,28 @@ def real_complx_mult(num_1, num_2):
     real_prt = term1 + term4
     complx_prt = term2 + term3
 
-    # NOTE TO MYSELF: What if there is a result with a '-' as the middle sign?
-    return str(real_prt) + ' + ' + str(complx_prt) + 'i'
+    if str(complx_prt)[0] != '-':
+        return str(real_prt) + '+' + str(complx_prt) + 'i'
+
+    return str(real_prt) + str(complx_prt) + 'i'
 
 def real_complx_div(num_1, num_2):
     real_prt2, complx_prt2 = real_complx_sepration(num_2)
     complx_conjugate = str(real_prt) + ' + ' + str(complx_prt) + 'i'
+
     return result
 
 if __name__ == '__main__':
     num_1 = '4+8i'
-    num_2 = '5+3i'
-    num_1 = '-4-8i'
-    real_prt1, complx_part1 = real_complx_sepration(num_1)
-    print('The real part is: ', real_prt1)
-    print('The complex part is: ', complx_part1)
-    print('The middle sign is: ', search_middle_sign(num_1))
-    # result = real_complx_sum(num_1, num_2)
-    # print('The result of the sum is: ', result)
-    # result = real_complx_subs(num_1, num_2)
-    # print('The result of the subtraction is: ', result)
-    # result = real_complx_mult(num_1, num_2)
-    # print('The result of the multiplication is: ', result)
+    num_2 = '5+10i'
+    # num_1 = '-4-8i'
+    # real_prt1, complx_part1 = real_complx_sepration(num_1)
+    # print('The real part is: ', real_prt1)
+    # print('The complex part is: ', complx_part1)
+    # print('The middle sign is: ', search_middle_sign(num_1))
+    result = real_complx_sum(num_1, num_2)
+    print('The result of the sum is: ', result)
+    result = real_complx_subs(num_1, num_2)
+    print('The result of the subtraction is: ', result)
+    result = real_complx_mult(num_1, num_2)
+    print('The result of the multiplication is: ', result)
