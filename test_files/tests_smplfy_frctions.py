a8aafde762be0dfd396f8b069dec4e00208580a6
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-06-10 23:42:33 -0500

    Correctd bug in calc.smplfy_frctions()

--- test_files/tests_smplfy_frctions.py
+++ test_files/tests_smplfy_frctions.py
@@ -12,6 +12,18 @@ def test_smplfy_frctions_1():
     assert uppr_part == 2
     assert lwr_part == 3
 
+def test_smplfy_frctions_2():
+    uppr_part, lwr_part = 25, 50
+    uppr_part, lwr_part = smplfy_frctions(uppr_part, lwr_part)
+    assert uppr_part == 1
+    assert lwr_part == 2
+
+def test_smplfy_frctions_3():
+    uppr_part, lwr_part = 6, 12
+    uppr_part, lwr_part = smplfy_frctions(uppr_part, lwr_part)
+    assert uppr_part == 1
+    assert lwr_part == 2
+
 if __name__ == '__main__':
     os.chdir('..') # For being able to debug while testing
     sys.exit(pytest.main(['-k', 'tests_smplfy_frctions.py'], plugins=[test_smplfy_frctions_1()]))
