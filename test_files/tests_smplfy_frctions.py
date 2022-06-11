1854aa165b393bf04c866f2889877b86a6306832
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-06-10 23:22:42 -0500

    Updtd tests_smplfy_frctions 4 debug whil testing

--- test_files/tests_smplfy_frctions.py
+++ test_files/tests_smplfy_frctions.py
@@ -1,13 +1,17 @@
 import sys
 import os
+os.chdir('..')
 sys.path.append(os.getcwd())
 
-from complx_num_algbra.calculator import *
+from calculator import *
 
-'''Run this file from the parent directory with "pytest test_files\tests_smplfy_frctions.py" '''
+import pytest
 
 def test_smplfy_frctions_1():
     uppr_part, lwr_part = 8, 12
     uppr_part, lwr_part = smplfy_frctions(uppr_part, lwr_part)
     assert uppr_part == 2
     assert lwr_part == 3
+
+if __name__ == '__main__':
+    sys.exit(pytest.main(['-k', 'tests_smplfy_frctions.py'], plugins=[test_smplfy_frctions_1()()]))
